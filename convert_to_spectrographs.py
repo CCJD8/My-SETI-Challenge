8c084f716ead6e6beeb03b6fa357dea9a48274a1
Connor <39064009+CCJD8@users.noreply.github.com>
2025-08-14 19:37:45 +0100

    vscode saviour from local backup

--- convert_to_spectrographs.py
+++ convert_to_spectrographs.py
@@ -8,11 +8,20 @@ import getopt
 
 
 def take_spectrogram_log(spectrogram, log_option):
-    """Take logarithm of spectrograph."""
+    """Take logarithm of spectrogram. Ensure that there are no values less than or equal to zero."""
     if log_option:
+        spectrogram_pos_min = spectrogram[spectrogram > 0].min()
+        spectrogram[spectrogram <= 0] = spectrogram_pos_min
         return np.log(spectrogram)
     else:
         return spectrogram
+
+
+def to_json(header):
+    """Convert header (type: python dict) to json."""
+    uuid = header['uuid']
+    signal_classification = header['signal_classification']
+    return {'uuid':uuid, 'signal_classification':signal_classification}
 
 
 def main(argv):
@@ -21,6 +30,9 @@ def main(argv):
     output_dir = '.'
     log_option = True
     usagestring = "convert_to_spectrographs.py -i <indir> -o <outdir> -l <logOpt>"
+    my_data_folder = os.getcwd() + '/'
+    if os.path.exists(my_data_folder) is False:
+        os.makedirs(my_data_folder)
     
     # Use getopt() to parse command line options and parameter list. 
     # The first return is a list of (option, value) pairs, 
@@ -50,7 +62,7 @@ def main(argv):
     
     
     # Read the .zip file with zipfile
-    dataset = zipfile.ZipFile("C:/Users/Connor/Documents/Nebuchadnezzar/Learning/Python/Summer-2025/Simulated Data Zip.zip")
+    dataset = zipfile.ZipFile("C:/Users/Connor/Documents/Nebuchadnezzar/Learning/Python/Summer-2025/Simulated-Data-Zip.zip")
     all_data_files = dataset.namelist()
     
     print('Input dir is ', input_dir)
@@ -60,9 +72,15 @@ def main(argv):
     # Create figure and axes for spectrogram
     fig, ax = plt.subplots(figsize=(10, 5))
     
+    all_headers_dict = {}
+        
     # Create spectrograms by reading .dat files with ibmseti and save as png
-    for current_file in all_data_files:
-        spectrogram = ibmseti.compamp.SimCompamp(dataset.open(current_file, 'r').read()).get_spectrogram()
+    for idx, current_file in enumerate(all_data_files):
+        if idx >= 20:
+            break
+        read_file = ibmseti.compamp.SimCompamp(dataset.open(current_file, 'r').read())
+        spectrogram = read_file.get_spectrogram()
+        header = read_file.header()
         ax.imshow(take_spectrogram_log(spectrogram, log_option), aspect=(0.5*float(spectrogram.shape[1]) / spectrogram.shape[0]), cmap="gray")
         ax.yaxis.set_inverted(False)
         
@@ -75,7 +93,68 @@ def main(argv):
         fig.savefig(os.path.join(output_dir, pngname), bbox_inches='tight')
         
         ax.cla()
+        
+        print(header)
+        
+        all_headers_dict.setdefault(header['signal_classification'], []).append(header)
+                
 
+    # Create full set of training and test data. Ensure there is the same amount of each signal type
+    training_percentage = 0.75
+    test_percentage = 0.25
+    full_training_set = {}
+    full_test_set = {}
+        
+    # Loop through dictionary which contains all data headers
+    # NB: [ERROR] we get duplicates in the test and train data when the training or testing sizes round to zero
+    for signal_type, uuid in all_headers_dict.items():
+        total_size = len(signal_type)
+        training_size = int(total_size * training_percentage)
+        test_size = int(total_size * test_percentage)
+        
+        training_set = uuid[:training_size]
+        test_set = uuid[-1*test_size:]
+        
+        full_training_set[signal_type] = training_set
+        full_test_set[signal_type] = test_set
+        
+            
+    max_zip_file_size_in_mb = 25
+    
+    # Create folders for png and zip files
+    png_file_folder = my_data_folder + "/pngfiles"
+    if os.path.exists(png_file_folder) is False:
+        os.makedirs(png_file_folder)
+    zip_file_folder = my_data_folder + "/zipfiles/"
+    if os.path.exists(zip_file_folder) is False:
+        os.makedirs(zip_file_folder)
+    
+    
+    # header_list is a list. Each item in the list is a header dictionary of a particular spectrogram
+    # TRAINING: Create the .zip files which contain the .png spectrograms.
+    for signal_type, header_list in full_training_set.items():
+        file_names = []
+        count = 1
+        
+        for i in range(len(header_list)):
+            name = png_file_folder + '/' + header_list[i]['uuid'] + ".png"
+            file_names.append(name)
+
+        for file in file_names:
+            print(zip_file_folder)
+            archive_name = f"{zip_file_folder}_{count}_{signal_type}.zip"
+            #print(f"You are attempting to save to:\n{archive_name}")
+            if os.path.exists(archive_name):
+                zz = zipfile.ZipFile(archive_name, mode='a')
+            else:
+                zz = zipfile.ZipFile(archive_name, mode='w')
+                
+            zz.write(file)
+            zz.close()
+            
+            if os.path.getsize(archive_name) > max_zip_file_size_in_mb * 1024 ** 2:
+                count += 1
+                
 
 # Ensure the program is run only when run directly by checking 
 # if __name__ == "__main__". If true, the main() function is called 
